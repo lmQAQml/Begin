09857459ac5089584c6a22a4c90f2c79cc787a77
lmQAQml <lmQAQml@163.com>
2022-04-17 02:48:15 +0800

    练习\

---
+++ everyDay/src/day09/Day09.java
@@ -0,0 +1,20 @@
+package src.day09;
+
+import java.util.Scanner;
+
+/**
+ * 返回输入的最后一个字符串长度
+ */
+public class Day09 {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        if(sc.hasNextLine()) {
+            String last = sc.nextLine();
+            sb.append(last);
+        }
+        String str = sb.reverse().toString();
+        String[] s = str.trim().split(" ");
+        System.out.print(s[0].length());
+    }
+}
