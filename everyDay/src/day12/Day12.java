6f8e8e8659de48ad8ed34e86185ec91abd54bb09
lmQAQml <lmQAQml@163.com>
2022-04-26 02:34:19 +0800

    补习

--- everyDay/src/day12/Day12.java
+++ everyDay/src/day12/Day12.java
@@ -1,9 +1,11 @@
 package src.day12;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Day12 {
     public static void main(String[] args) throws Exception {
         Integer result = ((Callable<Integer>) () -> 2+1).call();
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
     }
 }
