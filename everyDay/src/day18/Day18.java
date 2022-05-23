636a79bdd7d097696d62c603ee8c0feea883e776
lmQAQml <lmQAQml@163.com>
2022-05-23 22:24:45 +0800

    练习

--- everyDay/src/day18/Day18.java
+++ everyDay/src/day18/Day18.java
@@ -1,4 +1,11 @@
 package src.day18;
 
 public class Day18 {
+
+    public static void main(String[] args) {
+        long num = (int) 3.9;
+        System.out.println(num);
+        num %= 2;
+        System.out.println(num);
+    }
 }
