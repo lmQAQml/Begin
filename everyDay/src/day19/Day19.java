a4d266ef1b9e44c254fa5eb04bf67eea2b09e28e
lmQAQml <lmQAQml@163.com>
2022-05-25 22:58:48 +0800

    练习

---
+++ everyDay/src/day19/Day19.java
@@ -0,0 +1,28 @@
+package src.day19;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Day19 {
+    static int time = 1;
+    public static void main(String[] args) {
+
+        HashMap<Object, Integer> map = new HashMap<>();
+        map.put("key", time);
+        new Thread(() -> visit(map)).start();
+        new Thread(() -> visit(map)).start();
+    }
+
+    private static void visit(HashMap map) {
+        synchronized (map) {
+            Object key = map.get("key");
+            if (Objects.nonNull(key)) {
+                map.put("key", time);
+                key = map.get("key");
+            }
+            System.out.println(key);
+            map.put("key", ++time);
+        }
+    }
+}
