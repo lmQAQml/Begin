356e1d5a98dc5bdb6161f89eaf95c2bd4532b404
lmQAQml <lmQAQml@163.com>
2022-05-27 03:24:47 +0800

    练习

--- everyDay/src/day20/Day20.java
+++ everyDay/src/day20/Day20.java
@@ -1,7 +1,9 @@
 package src.day20;
 
-import javax.swing.text.html.parser.Entity;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Day20 {
 
@@ -12,14 +14,29 @@ public class Day20 {
         // 泛型类的调用
         Factory<Car> factory = new Factory<>(new Car("Audi", new BigDecimal(100000)));
 
+
+        Integer[] nums = new Integer[] {1, 2, 3};
+        List<Integer> list = Arrays.asList(nums);
+        nums[0] = 0;
+        // Arrays.asList()方法只是将数组的引用改变，所以对原数组进行变更，会导致此时转换后的list也发生变更.注意这里的list并不是平时用到的list，而是一个内部类的ArrayList
+        System.out.println(list);
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.remove(1);
+        System.out.println(arrayList);
     }
 
     /**
      * 泛型方法
+     *
      * @param t
      * @param <T>
      */
-    public static  <T> void run(T t) {
+    public static <T> void run(T t) {
         System.out.println(t.getClass());
         System.out.println(t);
     }
