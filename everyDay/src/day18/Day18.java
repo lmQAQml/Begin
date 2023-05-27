9d34f3fcc3fcb9d33feb42f66d0e06e839809290
lmQAQml <lmQAQml@163.com>
2023-05-27 12:10:46 +0800

    取整

--- everyDay/src/day18/Day18.java
+++ everyDay/src/day18/Day18.java
@@ -2,10 +2,16 @@ package src.day18;
 
 public class Day18 {
 
+    /**
+     * 负数向上，大于等于0的数向下
+     * @param args
+     */
     public static void main(String[] args) {
-        long num = (int) 3.9;
+        int num = (int) 3.9;
         System.out.println(num);
         num %= 2;
         System.out.println(num);
+        int num2 = (int) -3.9;
+        System.out.println(num2);
     }
 }
