3771dada6362d8f8d46818e291128c2ac2e1c04f
lmQAQml <lmQAQml@163.com>
2024-04-24 12:16:38 +0800

    +redis分布式锁, 算法学习

--- everyDay/src/day11/Day11.java
+++ everyDay/src/day11/Day11.java
@@ -1,7 +1,29 @@
 package day11;
 
+import java.util.Arrays;
+
 public class Day11 {
     public static void main(String[] args) {
+        int[] g = new int[] {1,2,3};
+        int[] s = new int[] {3};
+        System.out.println(findContentChildren(g, s));
     }
 
+    public static int findContentChildren(int[] g, int[] s) {
+        int res = 0;
+        Arrays.sort(s);
+        Arrays.sort(g);
+        // 循环结束条件：孩子都喂饱了或者饼干用完了
+        for(int cookie=0,child=0;child<g.length && cookie<s.length;cookie++) {
+            // 如果满足，移动饼干和孩子
+            // 不满足，移动饼干，去找下一个满足的饼干
+            // 为什么可以用贪心？如果当前的饼干满足不了这个孩子，当然也满足不了比这个孩子胃口更大的孩子
+            // 所以这块饼干可以跳过了
+            if(g[child]<=s[cookie]) {
+                res++;
+                child++;
+            }
+        }
+        return res;
+    }
 }
--- src/main/java/com/app/utils/RedisUtil.java
+++ src/main/java/com/app/utils/RedisUtil.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -14,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class RedisUtil {
 
     @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private RedisTemplate redisTemplate;
 
     /**
      * 指定key设置过期时间
@@ -24,7 +25,14 @@ public class RedisUtil {
      * @return
      */
     public boolean expire(String key, long time) {
-        return redisTemplate.expire(key, time, TimeUnit.SECONDS);
+        try {
+            if (time > 0)
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     /**
@@ -50,17 +58,65 @@ public class RedisUtil {
         return redisTemplate.opsForValue().get(key);
     }
 
-    public boolean setNX(String key, String expireTime) {
+    /**
+     * 使用setIfAbsent()方法实现锁
+     *
+     * @param key
+     * @param expireTime
+     * @return
+     */
+    public Boolean setNX(String key, String expireTime) {
         // 锁不存在，创建即可
         if (redisTemplate.opsForValue().setIfAbsent(key, expireTime)) {
             return true;
         }
         // 判断所是否过期
         if (!ObjectUtils.isEmpty(expireTime) && Long.parseLong(expireTime) < System.currentTimeMillis()) {
-            String oldTime = redisTemplate.opsForValue().getAndSet(key, expireTime);
+            String oldTime = (String) redisTemplate.opsForValue().getAndSet(key, expireTime);
             return !ObjectUtils.isEmpty(oldTime) && Objects.equals(oldTime, expireTime);
         }
         return false;
     }
+
+    /**
+     * 实现分布式锁
+     *
+     * @param key
+     * @param expireTime
+     * @return
+     */
+    public Boolean setNX(String key, String value, long expireTime) {
+        // 锁不存在
+        if (redisTemplate.opsForValue().setIfAbsent(key, value, expireTime, TimeUnit.SECONDS)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 释放锁
+     * @param keys
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public void delLock(String... keys) {
+        if (keys != null && keys.length > 0) {
+            if (keys.length == 1) {
+                redisTemplate.delete(keys);
+            }
+            redisTemplate.delete(keys);
+        }
+    }
+
+    /**
+     * 解决重复消费问题(未保证原子性), 配合定时任务
+     * 分数存时间戳+过期时间
+     * @return
+     */
+    public Boolean storeMessageInfo(String key, String value, double time) {
+        Boolean hasAdd = redisTemplate.opsForZSet().add(key, value, time);
+        return hasAdd;
+    }
+
 
 }
